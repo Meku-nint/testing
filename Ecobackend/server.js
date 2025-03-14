3a49d57708712abe537d7f1d88a0919d7cc74126
Meku-nint <mw908334@gmail.com>
2025-03-14 11:18:53 +0300

    coco

--- Ecobackend/server.js
+++ Ecobackend/server.js
@@ -52,8 +52,10 @@ const startServer = async () => {
     try {
         await connectDB();
         app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
+        console.error('Failed to start server:', error);
     }
 };
 startServer();
